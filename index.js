91826021fe94f95c0a15209f234881cb4ddeff3a
Dharti Madeka <dpmadeka@dons.usfca.edu>
2020-10-25 15:50:46 -0700

    make it better

---
+++ index.js
@@ -0,0 +1,42 @@
+const webSocketsServerPort = 8000;
+const webSocketServer = require('websocket').server;
+const http = require('http');
+
+//create http server
+const server = http.createServer();
+server.listen(webSocketsServerPort);
+console.log("listening on port" + webSocketsServerPort);
+
+const webSocket = new webSocketServer({
+    httpServer : server
+});
+
+const clients = {};
+
+//get unique userID for each user
+const getUniqueUserID = () => {
+    const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    return s4()+s4()+'-'+s4();
+};
+
+webSocket.on('request', function(request){
+    var userID = getUniqueUserID();
+    console.log((new Date())+ ' Received a new connection from origin ' + request.origin + ".");
+    //accept the request
+    const connection = request.accept(null, request.origin);
+    clients[userID] = connection;
+    console.log('connected ' + userID + 'in' + Object.getOwnPropertyNames(clients));
+    connection.on('message', function(message){
+        if(message.type === 'utf8')
+        {
+            console.log('Received msg: ' + message.utf8Data);
+            //broadcast message to all
+            for(key in clients)
+            {
+                clients[key].sendUTF(message.utf8Data);
+                console.log("send message to " + clients[key]);
+            }
+        }
+    })
+});
+
